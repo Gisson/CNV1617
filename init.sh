8be08cbf0194eb72505e684b0b03cca7d0e41ad4
Jorge Heleno <jorge.heleno@tecnico.ulisboa.pt>
2017-04-11 01:34:28 +0100

    Add instrumentation
    Instrumentation works but for some reason it fail and send stack trace when ran.

--- init.sh
+++ init.sh
@@ -2,6 +2,10 @@
 
 BASEDIR="$HOME/CNV1617"
 BITURL="http://grupos.tecnico.ulisboa.pt/~meic-cnv.daemon/labs/labs-bit/BIT.zip"
+BITDIR="$HOME/BIT"
+
+#This is specific for the aws machine
+#export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk"
 
 cd $BASEDIR
 
@@ -12,9 +16,12 @@ if ! [[ -d  "$HOME/BIT" ]];then
   cd "$HOME"
   wget $BITURL
   unzip BIT.zip
+  source "$BITDIR/java-config.sh"
   popd
 fi
 
 cd "server/"
 
+#create instrumentation dirs for raytracer
+mkdir -p  "$BASEDIR/raytracer/src/raytracer/instrumented/raytracer"
 make run
